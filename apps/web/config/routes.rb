3621601dfc1e22d59cc6dc9d33e1586539d0150a
Aleksandr Dyuzhikov <alex.duzh@gmail.com>
2019-04-30 04:00:05 +0300

    Add home page and fond reports page

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 # Configure your routes here
 # See: http://hanamirb.org/guides/routing/overview/
 #
 # Example:
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
+
+root to: 'home#index'
+get '/fond_reports', to: 'fond_reports#index', as: :fond_reports
