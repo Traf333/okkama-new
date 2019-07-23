f5429fa5f4a3de76f62dd2018bd8e20803c399a3
Aleksandr Dyuzhikov <alex.duzh@gmail.com>
2019-07-23 16:44:26 +0300

    Delete surname attribute from mail chimp type

--- lib/okkama/entities/report_item/mail_chimp.rb
+++ lib/okkama/entities/report_item/mail_chimp.rb
@@ -2,13 +2,7 @@
 
 module ReportItem
   class MailChimp < ReportItem::Base
-    attr_reader :surname
-
     private
-
-    def post_initialize(params)
-      @surname = params[:surname]
-    end
 
     def match_by_name?(transaction)
       name_not_empty? &&
