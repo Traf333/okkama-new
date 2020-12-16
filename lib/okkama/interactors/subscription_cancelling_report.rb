d74698c9537171773b881cf1322015a5bbfe948d
Igor Trofimov <igortr333@gmail.com>
2020-12-16 15:40:30 +0200

    one more attempt

--- lib/okkama/interactors/subscription_cancelling_report.rb
+++ lib/okkama/interactors/subscription_cancelling_report.rb
@@ -12,11 +12,11 @@ class SubscriptionCancellingReport
     @transactions = []
     @reports = []
     puts 'reading transactions======='
-    CSV.foreach(params[:transactions][:tempfile].path, headers: true, col_sep: ';', encoding: 'utf-8') do |row|
+    CSV.parse(params[:transactions][:tempfile].read.force_encoding('UTF-8'), headers: true, col_sep: ';', encoding: 'utf-8') do |row|
       @transactions << row.to_hash.slice('Email', 'Статус')
     end
 
-    options = { headers: true, encoding: 'utf-8' }
+    options = { headers: true }
     options[:col_sep] = ';' unless params[:type_report] == 'report_mail_chimp'
 
     puts 'reading reportss======='
